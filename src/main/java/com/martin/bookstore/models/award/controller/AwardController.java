6ff3bb733552f62f08156d3c8dfbc8fdee9c0675
martin-aghajanian <martin.aghajanian@gmail.com>
2025-04-10 11:50:26 +0400

    implement basic endpoints and dto class for awards

--- src/main/java/com/martin/bookstore/models/award/controller/AwardController.java
+++ src/main/java/com/martin/bookstore/models/award/controller/AwardController.java
@@ -1,5 +1,44 @@
 package com.martin.bookstore.models.award.controller;
 
+import com.martin.bookstore.models.award.dto.AwardDto;
+import com.martin.bookstore.models.award.service.AwardService;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/awards")
 public class AwardController {
+
+    private final AwardService awardService;
+
+    public AwardController(AwardService awardService) {
+        this.awardService = awardService;
+    }
+
+    @GetMapping
+    public List<AwardDto> getAllAwards() {
+        return awardService.getAllAwards();
+    }
+
+    @GetMapping("/{id}")
+    public AwardDto getAwardById(@PathVariable Long id) {
+        return awardService.getAwardById(id);
+    }
+
+    @PostMapping
+    public AwardDto createAward(@RequestBody AwardDto awardDto) {
+        return awardService.createAward(awardDto);
+    }
+
+    @PutMapping("/{id}")
+    public AwardDto updateAward(@PathVariable Long id, @RequestBody AwardDto awardDto) {
+        return awardService.updateAward(id, awardDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAward(@PathVariable Long id) {
+        awardService.deleteAward(id);
+    }
+
 }
